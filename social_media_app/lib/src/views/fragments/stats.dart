40ae3f6c9cdeb5ebeba7fbffffb0d3883cec10d7
Raghav <raghav.agrwl@Raghavs-MacBook-Pro.local>
2021-06-27 03:05:28 +0530

    Stats UI Added

--- social_media_app/lib/src/views/fragments/stats.dart
+++ social_media_app/lib/src/views/fragments/stats.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_app/social_media.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class Statistics extends StatelessWidget {
-  var titleList = ["Ghaziabad", "Punjab", "Agra"];
-
-  var imageList = [
-    Assets.loginPageImage,
-    Assets.signupPageImage,
-    Assets.welcomePageImage,
-  ];
+// ignore: camel_case_types
+class Statistics extends StatefulWidget {
+  //const stats({Key? key}) : super(key: key);
 
   @override
+  _StatisticsState createState() => _StatisticsState();
+}
+
+var titleList = [
+  "Punjab",
+  "Ghaziabad",
+  "South Delhi",
+  "Moradabad",
+  "Allahabad",
+  "Lucknow",
+  "Pune",
+  "Mumbai",
+  "Himachal",
+  "Haryana"
+];
+var imageList = [
+  "images/login.png",
+  "images/login.png",
+  "images/login.png",
+  "images/login.png",
+  "images/login.png",
+  "images/login.png",
+  "images/login.png",
+  "images/login.png",
+  "images/login.png",
+  "images/login.png"
+];
+
+// ignore: camel_case_types
+class _StatisticsState extends State<Statistics> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
+    return Scaffold(
+      body: ListView.builder(
         itemCount: titleList.length,
         itemBuilder: (context, index) {
           return GestureDetector(
-            onTap: () {
-              showDialogFunc(context, imageList[index], titleList[index]);
-            },
+            onTap: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) {
+              return GraphPlot();
+            })),
             child: Card(
               child: Row(
                 children: <Widget>[
@@ -52,53 +80,59 @@ class Statistics extends StatelessWidget {
       ),
     );
   }
+}
 
-  showDialogFunc(context, img, title) {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return Center(
-          child: Material(
-            type: MaterialType.transparency,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              padding: EdgeInsets.all(15),
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.8,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: Image.asset(
-                      img,
-                      width: 200,
-                      height: 200,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
+class GraphPlot extends StatefulWidget {
+  @override
+  _GraphPlotState createState() => _GraphPlotState();
+}
+
+class _GraphPlotState extends State<GraphPlot> {
+  List<GraphData> _chartData;
+  TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: SfCircularChart(
+      title: ChartTitle(text: 'Title'),
+      legend:
+          Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+      tooltipBehavior: _tooltipBehavior,
+      series: <CircularSeries>[
+        RadialBarSeries<GraphData, String>(
+            dataSource: _chartData,
+            xValueMapper: (GraphData data, _) => data.graphFactors,
+            yValueMapper: (GraphData data, _) => data.graphValue,
+            dataLabelSettings: DataLabelSettings(isVisible: true),
+            enableTooltip: true,
+            maximumValue: 40000)
+      ],
+    )));
+  }
+
+  List<GraphData> getChartData() {
+    final List<GraphData> chartData = [
+      GraphData('Closed Issues', 2900),
+      GraphData('Active Issues', 23050),
+      GraphData('Total Issues Created', 24880),
+      GraphData('Active NGOs', 24880),
+      GraphData('Active Users', 34390),
+    ];
+    return chartData;
+  }
+}
+
+class GraphData {
+  GraphData(this.graphFactors, this.graphValue);
+  final String graphFactors;
+  final int graphValue;
 }
